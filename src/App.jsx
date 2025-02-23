767dad1702ecd88883847c9821ea28613234409d
Pras-hant-karki <gymratprashant911@gmail.com>
2025-02-23 17:46:46 +0545

    Add signin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import React, { useContext } from "react";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import { UserContext } from "./context/UserContext";
+import SigninPage from "./pages/SigninPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
+  const { user } = useContext(UserContext);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/signin"
+          element={user ? <Navigate to="/" /> : <SigninPage />}
+        />
+        <Route
+          path="/signup"
+          element={user ? <Navigate to="/" /> : <SignupPage />}
+        />
       </Routes>
     </Router>
   );
