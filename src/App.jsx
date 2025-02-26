ccbaf43ff098cb0281d7d217b4b07b7d8422d268
Pras-hant-karki <gymratprashant911@gmail.com>
2025-02-26 20:58:34 +0545

    Fix routing and tabs

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,23 @@ import LandingPage from "./pages/LandingPage";
 import { UserContext } from "./context/UserContext";
 import SigninPage from "./pages/SigninPage";
 import SignupPage from "./pages/SignupPage";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  const { user } = useContext(UserContext);
+  const { user, isLoading } = useContext(UserContext);
+
+  if (isLoading) {
+    return (
+      <div className="min-h-[100vh] w-full bg-light-bg dark:bg-dark-bg flex items-center justify-center">
+        Loading...
+      </div>
+    );
+  }
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={user ? <MainPage /> : <LandingPage />} />
         <Route
           path="/signin"
           element={user ? <Navigate to="/" /> : <SigninPage />}
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,139 @@
+import axios from "axios";
+import React, { useState } from "react";
+import HomepageBody from "../components/Homepage/HomepageBody";
+import ProfileBody from "../components/Profile/ProfileBody";
+import SearchBody from "../components/SearchPage/SearchBody";
+import Header from "../components/common/Header";
+import Sidebar from "../components/common/Sidebar";
+import { ToastContainer } from "react-toastify";
+import AddPostModal from "../components/modal/AddPostModal";
+import WorkoutRequests from "../components/WorkoutRequests/WorkoutRequests";
+import AppointmentRequests from "../components/AppointmentRequests/AppointmentRequests";
+import PostDetails from "../components/Homepage/PostDetails";
+
+const MainPage = () => {
+  const [activeTab, setActiveTab] = useState("home");
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [userInfo, setUserInfo] = useState(null);
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+    setSelectedPost(null);
+    localStorage.setItem("activeTab", tabName);
+  };
+
+  const handlePostClick = (post) => {
+    setSelectedPost(post);
+    setActiveTab("postDetails");
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const fetchUserInfo = async (userId) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/users/${userId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      ); // Replace with your API endpoint for fetching user information
+      if (response.status === 200) {
+        const userData = response.data;
+        setUserInfo(userData);
+      } else {
+        console.error("Failed to fetch user information");
+      }
+    } catch (error) {
+      console.error("Error fetching user information:", error);
+    }
+  };
+
+  const tabConfig = {
+    home: {
+      header: "All Posts",
+      pageInfo: "View all posts",
+      body: (
+        <HomepageBody
+          handlePostClick={handlePostClick}
+          userInfo={userInfo}
+          fetchUserInfo={fetchUserInfo}
+        />
+      ),
+    },
+    search: {
+      header: "Search Posts",
+      pageInfo: "Search for posts",
+      body: (
+        <SearchBody
+          handlePostClick={handlePostClick}
+          userInfo={userInfo}
+          fetchUserInfo={fetchUserInfo}
+        />
+      ),
+    },
+    appointmentRequests: {
+      header: "Appointment Requests",
+      pageInfo: "View your appointment requests",
+      body: <AppointmentRequests />,
+    },
+    workoutRequests: {
+      header: "Workout Requests",
+      pageInfo: "View your workout requests",
+      body: <WorkoutRequests />,
+    },
+    profile: {
+      header: "Profile",
+      pageInfo: "View your profile",
+      body: <ProfileBody userInfo={userInfo} fetchUserInfo={fetchUserInfo} />,
+    },
+    postDetails: {
+      header: "Post Details",
+      pageInfo: "View post details",
+      body: (
+        <PostDetails
+          post={selectedPost}
+          userInfo={userInfo}
+          fetchUserInfo={fetchUserInfo}
+        />
+      ),
+    },
+  };
+
+  const { header, pageInfo, body } = tabConfig[activeTab];
+
+  return (
+    <div className="flex w-full min-h-[100vh] bg-light-bg dark:bg-dark-bg">
+      <Sidebar activeTab={activeTab} handleTabClick={handleTabClick} />
+
+      <ToastContainer position="top-right" autoClose={3000} />
+
+      <div className="flex flex-col justify-between text-black dark:text-white p-6 w-full md-2:ml-[280px] md-2:flex-1 md-2:relative md-2:px-6 md-2:py-0 lg:px-10">
+        <div>
+          <Header
+            currentPage={header}
+            pageInfo={pageInfo}
+            handleTabClick={handleTabClick}
+            openModal={openModal}
+          />
+
+          {body}
+        </div>
+      </div>
+
+      {isModalOpen && (
+        <AddPostModal closeModal={closeModal} openModal={openModal} />
+      )}
+    </div>
+  );
+};
+
+export default MainPage;
